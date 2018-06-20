247c7b09378908abc63a9029dd4a4d426b6e13b1
gregp <gperlman27@gmail.com>
2018-06-19 21:46:32 -0700

    rendering react components

--- client/index.js
+++ client/index.js
@@ -1,1 +1,11 @@
-console.log('blahasdasadfsdfsdfsdfflkasdfjasd;asdfsadflaasdfeasdffefesdfdfkj');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>heasdfllo</div>
+    )
+}
+
+const appNode = document.getElementById("app");
+ReactDOM.render(<App />, appNode);
